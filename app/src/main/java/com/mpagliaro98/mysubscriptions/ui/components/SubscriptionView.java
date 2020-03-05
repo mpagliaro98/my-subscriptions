ef94ca4463383059bfdade5dec2810d4a05d8398
Michael Pagliaro <mhp5035@rit.edu>
2020-03-04 23:18:12 -0500

    Began base-work for building a UI component to display Subscriptions.

---
+++ app/src/main/java/com/mpagliaro98/mysubscriptions/ui/components/SubscriptionView.java
@@ -0,0 +1,11 @@
+package com.mpagliaro98.mysubscriptions.ui.components;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class SubscriptionView extends LinearLayout {
+
+    public SubscriptionView(Context context) {
+        super(context);
+    }
+}
