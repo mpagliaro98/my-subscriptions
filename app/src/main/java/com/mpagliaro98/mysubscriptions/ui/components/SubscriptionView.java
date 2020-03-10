ceeb7b8dfae3fdec7fddedb1e04c7a0dd06a0194
Michael Pagliaro <mhp5035@rit.edu>
2020-03-10 15:19:06 -0400

    Made subscription category colors visible on the main page.

--- app/src/main/java/com/mpagliaro98/mysubscriptions/ui/components/SubscriptionView.java
+++ app/src/main/java/com/mpagliaro98/mysubscriptions/ui/components/SubscriptionView.java
@@ -1,7 +1,9 @@
 package com.mpagliaro98.mysubscriptions.ui.components;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.mpagliaro98.mysubscriptions.R;
@@ -15,9 +17,6 @@ import java.util.Locale;
 public class SubscriptionView extends LinearLayout {
 
     private Subscription subscription;
-    private TextView textName;
-    private TextView textCost;
-    private TextView textNextDate;
 
     /**
      * Build this view off of a LinearLayout so we can utilize some of its properties,
@@ -40,9 +39,10 @@ public class SubscriptionView extends LinearLayout {
         inflater.inflate(R.layout.component_subscriptionview, this);
 
         // Get the TextViews that make up this component
-        textName = findViewById(R.id.subview_name);
-        textCost = findViewById(R.id.subview_cost);
-        textNextDate = findViewById(R.id.subview_startdate);
+        TextView textName = findViewById(R.id.subview_name);
+        TextView textCost = findViewById(R.id.subview_cost);
+        TextView textNextDate = findViewById(R.id.subview_startdate);
+        ImageView imageColor = findViewById(R.id.subview_color);
 
         // Set the text in each part of the component
         textName.setText(subscription.getName());
@@ -52,5 +52,7 @@ public class SubscriptionView extends LinearLayout {
         String nextDateStr = "Next Payment Date: " +
                 new SimpleDateFormat("MM/dd/yyyy", Locale.US).format(subscription.getNextPaymentDate());
         textNextDate.setText(nextDateStr);
+        imageColor.setColorFilter(context.getResources().getColor(subscription.getCategory().getColor()),
+                PorterDuff.Mode.SRC_IN);
     }
 }
